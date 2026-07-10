bafec4a0f0caf8f076db914a14b9f49a4bcd3210
dariiafilonenko <dariia.filonenko.25@kse.org.ua>
2026-07-10 19:19:59 +0300

    task 3

--- files_practice.py
+++ files_practice.py
@@ -17,19 +17,29 @@ import json
 #     writer = csv.DictWriter(file, fieldnames=["id", "amount", "category", "status"])
 #     writer.writeheader() # Обов'язково записуємо заголовки колонок
 #     writer.writerows(succ)
-failed = []
-with open("files/transactions.csv", "r", encoding="utf=8") as file:
-     reader = csv.DictReader(file)
-     for row in reader:
-         if row["status"] == "failed":
-             failed.append(row)
-with open("output files/failed_transactions.csv", "w", encoding="utf-8", newline="") as file:
-    writer = csv.DictWriter(file, fieldnames=["id", "amount", "category", "status"])
-    writer.writeheader()
-    writer.writerows(failed)
+# failed = []
+# with open("files/transactions.csv", "r", encoding="utf=8") as file:
+#      reader = csv.DictReader(file)
+#      for row in reader:
+#          if row["status"] == "failed":
+#              failed.append(row)
+# with open("output files/failed_transactions.csv", "w", encoding="utf-8", newline="") as file:
+#     writer = csv.DictWriter(file, fieldnames=["id", "amount", "category", "status"])
+#     writer.writeheader()
+#     writer.writerows(failed)
 
 
 
+with open("files/users.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+    for user in data:
+        if user["balance"] >= 1000:
+            user["status"] = "VIP"
+        else:
+            user["status"] = "Regular"
+with open("output files/new_users.json", "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=4)
+    print(data)
 
 
 
