34ed787aeda05d6420a913fe23e5907f02d31aae
dariiafilonenko <dariia.filonenko.25@kse.org.ua>
2026-07-10 18:42:47 +0300

    task 1

--- files_practice.py
+++ files_practice.py
@@ -2,3 +2,20 @@ import csv
 import json
 
 # Ваш код для завдань нижче:
+suma = 0
+succ = []
+with open("files/transactions.csv", "r", encoding="utf=8") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        try:
+            suma += float(row["amount"])
+            succ.append(row)
+        except ValueError:
+            continue
+print(succ)
+with open("output files/st.csv", "w") as file:
+    writer = csv.DictWriter(file, fieldnames=["id", "amount", "category", "status"])
+    writer.writeheader() # Обов'язково записуємо заголовки колонок
+    writer.writerows(succ)
+
+
